9a2c1f06f0bdbacf8733cde66aa2817253f4dd65
SyedTalha71x <syedtalha71x@gmail.com>
2025-07-07 17:16:06 +0500

    fix

--- client/src/pages/activities.jsx
+++ client/src/pages/activities.jsx
@@ -188,7 +188,7 @@ export default function Activities() {
                                         : 'text-[#616161] rounded-lg'
                                         }`}
                                 >
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-open h-4 w-4"><path d="M12 7v14"></path><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"></path></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="hidden sm:inline" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-open h-4 w-4"><path d="M12 7v14"></path><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"></path></svg>
                                     <span className="inline">Activiteitenbibliotheek</span>
                                 </button>
                             </div>
